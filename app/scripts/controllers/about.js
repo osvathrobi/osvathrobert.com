8dbcf9c5aee3d3bc2e61edbe5f4de17725d89fcd
Robert Osvath Boros <osvathrobi@yahoo.com>
2015-04-30 18:43:48 +0300

    Initial status

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name osvathrobiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the osvathrobiApp
+ */
+angular.module('osvathrobiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name osvathrobiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the osvathrobiApp
+ */
+angular.module('osvathrobiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
